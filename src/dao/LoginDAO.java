dfd5d288c16634e603fc4bda277ab2bbbfb94d94
yuki <yuki@LAPTOP-P34DNLJ8>
2019-11-20 18:39:17 +0900

    ログインDAOの作成途中

---
+++ src/dao/LoginDAO.java
@@ -0,0 +1,10 @@
+package dao;
+
+public class LoginDAO {
+
+	private final String JDBC_URL = "jdbc:mysql://localhost:3306/AoPlat" ;
+	private final String DB_USER = "root";
+	private final String DB_PASS = "1234";
+
+
+}
