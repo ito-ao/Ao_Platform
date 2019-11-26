aba29ee64a2b16e18330f9d9ed270a25f1e0629f
yuki <yuki@LAPTOP-P34DNLJ8>
2019-11-26 15:08:06 +0900

    11/26日現在 cssは読み込まれたが外部の画像を参照しているためかそれが読み込まれない（バックグラウンドの画像） 遷移先の画面はまだ作れていない

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -2,40 +2,62 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.LoginDAO;
+import model.Login;
+import model.Loginlogic;
+
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/LoginServlet")
+@WebServlet(name = "Login Servlet", urlPatterns = { "/Login Servlet" })
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response)
+					throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Login.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request
+			, HttpServletResponse response)
+					throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("name");
+		String pass = request.getParameter("pass");
+
+		Login login = new Login(name,pass);
+		Loginlogic bo = new Loginlogic();
+		boolean result = bo.execute(login);
+
+		LoginDAO d = new LoginDAO();
+
+		if(result) {
+			HttpSession session = request.getSession();
+			session.setAttribute("name",name);
+			session.setAttribute("id",d.findByLogin(login).getId());
+
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/jsp");
+			dispatcher.forward(request, response);
+		}else {
+			response.sendRedirect("/Platform/LoginServlet");
+		}
 	}
 
 }
